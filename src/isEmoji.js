67af7a6dac069cfed5c33bf8179f6207246299b4
Frazer Smith <frazer.dev@outlook.com>
2023-07-26 06:49:52 +0100

    chore(src): remove commented out code

--- src/isEmoji.js
+++ src/isEmoji.js
@@ -1,8 +1,5 @@
-// const variationSelectors = /[\uFE00-\uFE0F]/
 const isVariationSelector = (num) => num >= 65024 && num <= 65039
-// const highSurrogates = /[\uD800-\uDBFF]/
 const isHighSurrogate = (num) => num >= 55296 && num <= 56319
-// const lowSurrogates = /[\uDC00-\uDFFF]/
 const isLowSurrogate = (num) => num >= 56320 && num <= 57343
 
 // allow invisible characters in emojis
